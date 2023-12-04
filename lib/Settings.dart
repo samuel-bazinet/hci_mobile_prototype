48c41b093ed99ad3fad378840d28ba14e40a9f8a
Umairuddin <umairuddin.mohammed@ontariotechu.net>
2023-12-03 22:22:11 -0500

    Added map and barebone settings page

--- lib/Settings.dart
+++ lib/Settings.dart
@@ -1,17 +1,50 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: const Text('Settings'),
       ),
-      body: Center(
-        child: Text(
-          'This is the Settings Page',
-          style: TextStyle(fontSize: 20.0),
-        ),
+      body: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Account'),
+              onTap: () {
+                // Navigate to Account settings page
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Notifications'),
+              onTap: () {
+                // Navigate to Notifications settings page
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.security),
+              title: const Text('Privacy & Security'),
+              onTap: () {
+                // Navigate to Privacy & Security settings page
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text('Help & Support'),
+              onTap: () {
+                // Navigate to Help & Support page
+              },
+            ),
+          ],
+        ).toList(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: deprecated_member_use, library_prefixes
+
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
+import 'package:location/location.dart';
 import 'PlanRoute.dart';
 import 'Settings.dart';
 
@@ -14,22 +19,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Route Planner',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: HomePage(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: const Text('Home Page'),
         actions: [
           IconButton(
-            icon: Icon(Icons.settings),
+            icon: const Icon(Icons.settings),
             onPressed: () {
               Navigator.push(
                 context,
@@ -39,15 +48,11 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Page',
-          style: TextStyle(fontSize: 20.0),
-        ),
-      ),
+      body: const MapPage(),
       // Using ElevatedButton for navigation
       bottomNavigationBar: Padding(
-        padding: EdgeInsets.all(16.0), // Add padding to increase button size
+        padding:
+            const EdgeInsets.all(16.0), // Add padding to increase button size
         child: ElevatedButton(
           onPressed: () {
             Navigator.push(
@@ -56,8 +61,8 @@ class HomePage extends StatelessWidget {
             );
           },
           child: Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
+            padding: const EdgeInsets.all(15.0),
+            child: const Text(
               'Plan Route',
               style: TextStyle(fontSize: 18.0),
             ),
@@ -68,3 +73,61 @@ class HomePage extends StatelessWidget {
   }
 }
 
+// Map Page
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Location location = Location();
+  latLng.LatLng _center = const latLng.LatLng(
+      43.94645712813915, -78.89748808892554); // Default location
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
+
+  void retrieveLocation() async {
+    var currentLocation = await location.getLocation();
+    setState(() {
+      _center =
+          latLng.LatLng(currentLocation.latitude!, currentLocation.longitude!);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        FlutterMap(
+          mapController: MapController(),
+          options: MapOptions(
+            center: _center,
+            zoom: 18.0,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: const ['a', 'b', 'c'],
+            ),
+            MarkerLayer(markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: _center,
+                child: const Icon(Icons.location_on),
+              ),
+            ]),
+          ],
+        )
+      ],
+    ));
+  }
+}
