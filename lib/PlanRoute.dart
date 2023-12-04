8c703f54674dbe8a557e9e6e21957417f229ff3a
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-12-04 15:07:04 -0500

    Updated the plan route page

--- lib/PlanRoute.dart
+++ lib/PlanRoute.dart
@@ -1,46 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart' as latLng;
+import 'package:flutter_map/flutter_map.dart';
 import 'RouteList.dart';
 import 'Settings.dart';
 import 'Filter.dart';
 
 class PlanRoutePage extends StatelessWidget {
+  final startingPointController = TextEditingController();
+  final destinationController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Plan Route'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingsPage()),
-              );
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.filter_alt),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Filter()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'Plan your route here',
-          style: TextStyle(fontSize: 20.0),
+        appBar: AppBar(
+          title: const Text('Plan Route'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingsPage()),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.filter_alt),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Filter()),
+                );
+              },
+            ),
+          ],
         ),
-      ),
-      // Using an ElevatedButton for navigation
-      bottomNavigationBar: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: <Widget>[
+                  const Flexible(
+                    flex: 0,
+                    child:
+                        Padding(padding: EdgeInsets.all(5), child: Text('A:')),
+                  ),
+                  Flexible(
+                    flex: 8,
+                    child: TextField(
+                      controller: startingPointController,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: <Widget>[
+                  const Flexible(
+                    flex: 0,
+                    child:
+                        Padding(padding: EdgeInsets.all(5), child: Text('B:')),
+                  ),
+                  Flexible(
+                    flex: 8,
+                    child: TextField(
+                      controller: destinationController,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -56,9 +90,20 @@ class PlanRoutePage extends StatelessWidget {
                 ),
               ),
             ),
+            Expanded(
+                child: FlutterMap(
+                  mapController: MapController(),
+                  options: const MapOptions(keepAlive: true, initialZoom: 15, initialCenter:  latLng.LatLng(
+                      43.94645712813915, -78.89748808892554)),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      subdomains: const ['a', 'b', 'c'],
+                    ),
+                  ],
+                )),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
