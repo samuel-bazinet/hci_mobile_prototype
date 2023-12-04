4e80c0f8bb14efb446f735589febb6b345adde17
Umairuddin <umairuddin.mohammed@ontariotechu.net>
2023-12-03 22:38:45 -0500

    Optimized map so its no longer slow

--- lib/main.dart
+++ lib/main.dart
@@ -120,7 +120,7 @@ class _MapPageState extends State<MapPage> {
           ),
           children: [
             TileLayer(
-              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               subdomains: const ['a', 'b', 'c'],
             ),
             MarkerLayer(markers: [
